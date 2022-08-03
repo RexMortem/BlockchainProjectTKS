504772b016a7acf0dea0452b8b6a32ed556ee0d3
RexMortem <68436000+RexMortem@users.noreply.github.com>
2022-08-03 18:30:13 +0100

    Update SHA256.lua
    Added more structure for functions that we must write in the future

--- SHA256.lua
+++ SHA256.lua
@@ -2,4 +2,16 @@ local SHA256 = {
 	
 }
 
+function SHA256:Sign(Message, PrivateKey) -- returns unique signature for message
+	
+end
+
+function SHA256:Verify(Message, Signature, PublicKey) -- verifies whether signature valid
+	
+end
+
+function SHA256:GenerateKeys(Input)
+	
+end
+
 return SHA256
