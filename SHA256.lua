36c4b6ab488923e9fcc8148f563d371a3e38f5f8
RexMortem <68436000+RexMortem@users.noreply.github.com>
2022-08-04 14:49:54 +0100

    Update SHA256.lua
    Added comment; clarification as to what SHA-256 is

--- SHA256.lua
+++ SHA256.lua
@@ -1,3 +1,8 @@
+--[[
+	SHA stands for Secure Hashing Algorithm; a hashing algorithm is a one-way cryptographic function
+	Note that SHA-256 is the same thing as SHA-2; the 2 refers to the version, whereas 256 refers to the number of bits 
+--]]
+
 local SHA256 = {
 	
 }
